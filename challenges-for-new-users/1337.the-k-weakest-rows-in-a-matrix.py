e5acecda06fbc66c9cac8d86df6c3ab88913aa63
Angelo <angeloclaudio.re@gmail.com>
2022-04-21 23:19:26 +0200

    Solved problem 1337, 1342 and 1672

--- challenges-for-new-users/1337.the-k-weakest-rows-in-a-matrix.py
+++ challenges-for-new-users/1337.the-k-weakest-rows-in-a-matrix.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
 
 class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+    def kWeakestRows(self, mat: [[int]], k: int) -> [int]:
+        sumlist = list(map(sum,mat))
+        indexlist = list(range(len(sumlist)))
+        outt = sorted(list(zip(sumlist, indexlist)))
+        outl = [i[1] for i in outt ]
+
+        return outl[:k]
+
+[2,4,1,2,5]
+[0,1,2,3,4]
+
+mat = [[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]]
+k = 3
+
+pino = Solution()
+print(pino.kWeakestRows(mat,k))
+
--- challenges-for-new-users/1672.richest-customer-wealth.py
+++ challenges-for-new-users/1672.richest-customer-wealth.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+class Solution:
+    def maximumWealth(self, accounts: [[int]]) -> int:
+        return max(list(map(sum,accounts)))
+        #return max([sum(i) for i in accounts])
+
+pino = Solution()
+accounts = [[1,2,3],[3,2,1]]
+print(pino.maximumWealth(accounts))
+
+accounts = [[1,5],[7,3],[3,5]]
+print(pino.maximumWealth(accounts))
+
+accounts = [[2,8,7],[7,1,3],[1,9,5]]
+print(pino.maximumWealth(accounts))
